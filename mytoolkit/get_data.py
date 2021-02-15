561197f058a2385c15080f57e736f2efb1cda6aa
Magali Rousseau <rousseaumagali@gmail.com>
2021-02-15 15:53:01 +0100

    changed fucntion

--- mytoolkit/get_data.py
+++ mytoolkit/get_data.py
@@ -7,16 +7,16 @@ def clean_text(text):
   # remove punctuation
   text =  ''.join([e for e in text if e not in string.punctuation])
   # convert to lower case
-  text = text.lower()
-  # remove numbers
-  text = ''.join(word for word in text if not word.isdigit())
-  # remove stop words
-  stop_words = set(stopwords.words('english'))
-  word_tokens = word_tokenize(text)
-  text = [w for w in word_tokens if w not in stop_words]
-  # lemmatize
-  lemmatizer = WordNetLemmatizer()
-  text = ' '.join([lemmatizer.lemmatize(word) for word in text])
+  # text = text.lower()
+  # # remove numbers
+  # text = ''.join(word for word in text if not word.isdigit())
+  # # remove stop words
+  # stop_words = set(stopwords.words('english'))
+  # word_tokens = word_tokenize(text)
+  # text = [w for w in word_tokens if w not in stop_words]
+  # # lemmatize
+  # lemmatizer = WordNetLemmatizer()
+  # text = ' '.join([lemmatizer.lemmatize(word) for word in text])
   return text
 
 
